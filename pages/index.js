021e15782d82768254ea5994da32530d404afd10
jimspi <jpsthesecond@gmail.com>
2025-08-13 21:46:21 -0600

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,19 @@
+import Head from 'next/head';
+import NewsFlow from '../components/NewsFlow';
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>NewsFlow - Team Collaboration Platform</title>
+        <meta name="description" content="Replace chaotic email chains with organized team communication" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <NewsFlow />
+      </main>
+    </div>
+  );
+}
